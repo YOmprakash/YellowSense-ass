51cbf0dd56bfb3303c9e49de05e15eb1dc8727c9
YOmprakash <omprakashyamavaram@gmail.com>
2024-09-13 17:10:53 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 //App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Bookmarks from "./pages/Bookmarks";
-// import JobDetails from "./pages/JobDetails";
+import JobDetails from "./pages/JobDetails";
 import Jobs from "./pages/Jobs";
 
 function App() {
@@ -11,9 +11,9 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/" element={<Jobs />} />
-        {/* <Route path="/job/:id" element={<JobDetails />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/job/:id" element={<JobDetails />} />
         <Route path="/bookmarks" element={<Bookmarks />} />
       </Routes>
     </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
   return (
     <nav className="fixed bottom-0 z-10 w-full p-4 text-white bg-blue-500">
       <ul className="flex justify-between">
-        {/* <li>
-          <Link to="/" className="hover:underline">Home</Link>
-        </li> */}
         <li>
-          <Link to="/" className="hover:underline">Jobs</Link>
+          <Link to="/" className="hover:underline">Home</Link>
+        </li>
+        <li>
+          <Link to="/jobs" className="hover:underline">Jobs</Link>
         </li>
         <li>
           <Link to="/bookmarks" className="hover:underline">Bookmarks</Link>
